f07ee25a2b31a9548df1c4b5a6e5b852f17e57f6
arbind <arbindkamat10@gmail.com>
2024-07-19 12:12:35 +0545

    "package update"

--- lib/Views/album_details.dart
+++ lib/Views/album_details.dart
@@ -1,7 +1,5 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:techjartask/Service%20Provider/service_providers.dart';
 import '../App Providers/user_providers.dart';
 
 
